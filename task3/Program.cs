4eb29ebdbb5c2a9361b112da850d40c6f4263ba8
ognevok97 <ognevok97@mail.ru>
2022-11-21 23:51:11 +0700

    4 homework

---
+++ task3/Program.cs
@@ -0,0 +1,9 @@
+﻿/*Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8*/
+int N = new int ();
+Console.WriteLine ("Enter number");
+    N = Convert.ToInt32(Console.ReadLine());
+for (int i =2; i <= N; i++)
+if (i%2==0)
+Console.WriteLine(i);
